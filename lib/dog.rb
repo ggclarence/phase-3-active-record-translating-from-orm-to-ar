1ffb310fadc1423821619a43196f67a4e00ba494
Eric Van <ggclarence@yahoo.com>
2022-03-20 22:31:24 -0500

    commit tree 9cd4e5698e606120d3ffc953a137ddaadec78e12

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,17 @@
-class Dog
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/test.sqlite3"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+  )
+SQL
+ActiveRecord::Base.connection.execute(sql)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+class Dog < ActiveRecord::Base
 end
